94a238e30aa05787bd71437cbc19ddadb59d53d8
Džiugas Janulevičius <s034745@ad.viko.lt>
2021-04-30 11:42:50 +0300

    test

--- pages/lenteles.js
+++ pages/lenteles.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button  } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert  } from 'react-native';
 
 export default function Lenteles() {
 
   const atsakymas = 15.06;
 
   const [number, onChangeNumber] = React.useState(null);
-  
+
     return (
       <View style={styles.container}>
         <Text style={styles.header}>
         20% arbatpinigiai, pavalgius už 75.31€
         </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
-          placeholder="Atsakymas"
-          keyboardType="numeric"
-        />
-         <Button
-        title="Next"
-        //if ( atsakymas != value )
-      //  {
-       //   disabled 
-     //   }
-        onPress= { () => Alert.alert('Simple Button pressed') }  
-        />
+          <Text style={styles.help}>
+            (Atsakyme naudokite du skaičius po kakblelio)
+          </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder="Atsakymas"
+                 keyboardType="numeric"
+              />
+                <Button
+                  title="Next"
+                      onPress= { () => Alert.alert('Simple Button pressed') } 
+                   
+                />
       </View>
-
-
     );
 }
 
@@ -41,10 +39,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  header:{
+    fontSize: 24,
+    textAlign: 'center',
+    paddingBottom: 8
+  },
+  help:{
+    fontSize: 16,
+    textAlign: 'center',
+    paddingBottom: 8
+  },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 2,
+  },
+
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
   }
 });
 
