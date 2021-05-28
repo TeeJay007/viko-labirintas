d3edb7dba0cff86a90454a7183a0fdc435cbd127
Weazzzzzzey <heronightmares@gmail.com>
2021-05-28 15:41:11 +0300

    Pabaigta balionu uzduotis

--- pages/Balloons.js
+++ pages/Balloons.js
@@ -4,7 +4,8 @@ import {
   Text,
   View,
   Image,
-  ImageBackgroundBase,
+  TextInput,
+  LogBox,
   SafeAreaView,
   Dimensions,
   ImageBackground,
@@ -13,7 +14,8 @@ import {
 import "react-native-gesture-handler";
 
 
-
+const paperHeight = 192;
+const paperWidht = 130;
 
 class Balloons extends Component{
     
@@ -24,7 +26,10 @@ class Balloons extends Component{
             mainGreen: require('../Images/green1.png'),
             mainRed: require('../Images/red1.png'),
             imageHeight: 192,
-            imageWidth: 120,
+            imageWidth: 130,
+            numberOne: 0,
+            numberTwo: 0,
+            numberThree: 0,
         }
     }
 
@@ -40,7 +45,7 @@ class Balloons extends Component{
         }, 300);
         setTimeout(() => {
             this.setState({mainBlue: require('../Images/ketboard.png')})
-            this.setState({imageHeight: 150, imageWidth: 150})
+            this.setState({imageHeight: paperHeight, imageWidth: paperWidht})
         }, 500);
     };
 
@@ -55,8 +60,8 @@ class Balloons extends Component{
             this.setState({mainGreen: require('../Images/green4.png')})
         }, 300);
         setTimeout(() => {
-            this.setState({mainGreen: require('../Images/speekers.jpg')})
-            this.setState({imageHeight: 192, imageWidth: 150})
+            this.setState({mainGreen: require('../Images/twospeekers.jpg')})
+            this.setState({imageHeight: paperHeight, imageWidth: paperWidht})
         }, 500);
     };
 
@@ -72,27 +77,82 @@ class Balloons extends Component{
         }, 300);
         setTimeout(() => {
             this.setState({mainRed: require('../Images/mouse.jpg')})
-            this.setState({imageHeight: 150, imageWidth: 150})
+            this.setState({imageHeight: paperHeight, imageWidth: paperWidht})
         }, 500);
     };
 
+    numberOneChange(numberOne) {
+        if(numberOne == "-" || numberOne == " " || numberOne == "," || numberOne == ".")
+        this.setState({ numberOne: ""});
+        else
+        this.setState({ numberOne });
+    }
+
+    numberTwoChange(numberTwo) {
+        if(numberTwo == "-" || numberTwo == " " || numberTwo == "," || numberTwo == ".")
+        this.setState({ numberTwo: ""});
+        else
+        this.setState({ numberTwo });
+    }
+
+    numberThreeChange(numberThree) {
+        if(numberThree == "-" || numberThree == " " || numberThree == "," || numberThree == ".")
+        this.setState({ numberThree: ""});
+        else
+        this.setState({ numberThree });
+    }
+
+    handleSubmitCheckNumbers = () => {
+        
+        if(this.state.numberOne == "4" && this.state.numberTwo == "2" && this.state.numberThree == "1")
+        console.log("421");
+    }
+
     render(){
+        LogBox.ignoreAllLogs(true);
         return (
             <ImageBackground source={require('../Images/sa.jpg')} style={styles.image}>
                 <View style={styles.container}>
                     
-                    <TouchableOpacity onPress={this.onPressBlue}>
-                        <Image source={this.state.mainBlue} style={{width: this.state.imageWidth, height: this.state.imageHeight}}></Image>
+                    <View style={styles.textarea}>
+                    <Text style={{fontSize: 24, fontWeight: "bold"}}>Koks tai galėtų būti kabinetas?</Text>
+                    </View>
+                    
+                    <View style={styles.ballonsarea}>
+                    <TouchableOpacity style={styles.balloonsButton} onPress={this.onPressBlue}>
+                        <Image source={this.state.mainBlue} style={{width: this.state.imageWidth, height: this.state.imageHeight, borderRadius: 10}}></Image>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.onPressGreen}>
-                        <Image source={this.state.mainGreen} style={{width: this.state.imageWidth, height: this.state.imageHeight}}></Image>
+                    <TouchableOpacity style={styles.balloonsButton} onPress={this.onPressGreen}>
+                        <Image source={this.state.mainGreen} style={{width: this.state.imageWidth, height: this.state.imageHeight, borderRadius: 10}}></Image>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.onPressRed}>
-                        <Image source={this.state.mainRed} style={{width: this.state.imageWidth, height: this.state.imageHeight}}></Image>
+                    <TouchableOpacity style={styles.balloonsButton} onPress={this.onPressRed}>
+                        <Image source={this.state.mainRed} style={{width: this.state.imageWidth, height: this.state.imageHeight, borderRadius: 10}}></Image>
                     </TouchableOpacity>
-
+                    </View>
+                    
+                    <View style={styles.inputarea}>
+                        <Text style={{fontSize: 24, fontWeight: "bold"}} >Įrašykite:</Text>
+                        
+                        <View style={{flexDirection: 'row'}}>
+                        <TextInput style={styles.input} value={this.state.numberOne} placeholder="0" keyboardType='numeric' maxLength={1}
+                        onChangeText={(text) => this.numberOneChange(text)}
+                        />
+                        <TextInput style={styles.input} value={this.state.numberTwo} placeholder="0" keyboardType='numeric' maxLength={1}
+                        onChangeText={(text) => this.numberTwoChange(text)}
+                        />
+                        <TextInput style={styles.input} value={this.state.numberThree} placeholder="0" keyboardType='numeric' maxLength={1}
+                        onChangeText={(text) => this.numberThreeChange(text)}
+                        />
+                        </View>
+                        <Text></Text>
+                        <TouchableOpacity onPress={this.handleSubmitCheckNumbers}>
+                        <View style={styles.refreshButton}>
+                        <Text style={styles.refreshButtonText}>Patikrinti</Text>
+                        </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </ImageBackground>
         );
@@ -101,23 +161,72 @@ class Balloons extends Component{
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        flex: 1,
+        padding: 20,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    ballonsarea: {
+        height: 250,
+        flexDirection: 'row',
+        margin: 5,
+        
+        alignItems: 'center',
+    },
+    textarea: {
+        height: 100,
+        justifyContent: 'center',
+    },
+    balloonsButton: {
+        margin: 5,
+        borderRadius: 10,
+        height: 200,
+        width: 130,
     },
     balloon: {
         width: 120,
         height: 192,
     },
     ballonImage:{
-        width: 150,
-        height: 150,
+        width: 50,
+        height: 50,
     },
     image: {
         height: "100%",
         width: "100%",
         resizeMode: "cover",
+    },
+    inputarea: {
+        
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        padding: 10,
+        backgroundColor: "white",
+        borderColor: "gainsboro",
+        borderWidth: 7,
+        borderRadius: 20,
+        margin: 5,
+        marginBottom: 5,
+        fontSize: 25,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    refreshButtonText: {
+        fontSize: 24,
+        lineHeight: 24,
+        color: "white",
+        fontWeight: "bold",
+    },
+      refreshButton: {
+        width: 120,
+        height: 50,
+        backgroundColor: "dodgerblue",
+        marginLeft: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
     },
 });
 
