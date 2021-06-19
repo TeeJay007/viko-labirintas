e0b8886539d261ae42a41f16d90a3adc78a550d9
Weazzzzzzey <heronightmares@gmail.com>
2021-06-19 10:54:19 +0300

    Uzbaigta navigacija, pasalinti pertekliniai failai

--- pages/navigation_pages/penkidudu_vidus.js
+++ pages/navigation_pages/penkidudu_vidus.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, useWindowDimensions, TouchableOpacity } from 'react-native';
-
+import { useState } from 'react';
 import NavBase from './nav_base'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const IMAGE = require('../../NavigationCovers/penkiduduvidus_f.jpg');
+const IMAGE = require('../../NavigationCovers/penkidudunaujas.jpg');
 const IMAGE_WIDTH = 5708;
 const IMAGE_HEIGHT = 1538;
 
 export default function PenkiDuDuVidus({ navigation: { navigate }}) {
+    
+    const [washere, setwashere] = useState("-")
+    
+    AsyncStorage.getItem("seimas", (err, result) => {
+        setwashere(result);
+    });
+    
     const {width: screenW, height: screenH} = useWindowDimensions()
 
     const getScreenWidth = () => IMAGE_WIDTH / IMAGE_HEIGHT * screenH;
@@ -17,31 +25,62 @@ export default function PenkiDuDuVidus({ navigation: { navigate }}) {
     const resizeWidth = (w) => w * getScreenWidth() / IMAGE_WIDTH
     const resizeHeight = (h) => h * getScreenHeight() / IMAGE_HEIGHT
 
-    return (
-        <NavBase image={IMAGE} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}>
-            <TouchableOpacity onPress={() => {
-                navigate('proLanga')
-            }}  style={{
-                backgroundColor: 'rgba(255,255,255,0.5)',
-                opacity: 0.5,
-                padding: 10,
-                position: 'absolute',
-                top: resizeHeight(531),
-                left: resizeWidth(2709),
-                width: resizeWidth(693),
-                height: resizeHeight(570),
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <Text style={{
-                    color: 'black',
-                    fontWeight: 'bold',
-                    textAlign: 'center',
+    if(washere == 'false'){
+        return (
+            <NavBase image={IMAGE} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}>
+                <TouchableOpacity onPress={() => {
+                    navigate('proLanga')
+                }}  style={{
+                    //backgroundColor: 'rgba(255,255,255,0.5)',
+                    backgroundColor: 'white',
+                    opacity: 0.5,
+                    padding: 10,
+                    position: 'absolute',
+                    top: resizeHeight(600),
+                    left: resizeWidth(2200),
+                    width: resizeWidth(800),
+                    height: resizeHeight(570),
+                    justifyContent: 'center',
+                    alignItems: 'center'
                 }}>
-                    Pažvelk pro langą!
-                    UŽDUOTIS
-                </Text>
-            </TouchableOpacity>
-        </NavBase>
-    );
+                    <Text style={{
+                        color: 'black',
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                    }}>
+                        Pažvelk pro langą!
+                        UŽDUOTIS
+                    </Text>
+                </TouchableOpacity>
+            </NavBase>
+        );
+    } else {
+        return (
+            <NavBase image={IMAGE} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}>
+                <TouchableOpacity onPress={() => {
+                    navigate('penkiduketuri')
+                }}  style={{
+                    backgroundColor: 'white',
+                    opacity: 0.5,
+                    padding: 10,
+                    position: 'absolute',
+                    top: resizeHeight(1150),
+                    left: resizeWidth(150),
+                    width: resizeWidth(250),
+                    height: resizeHeight(250),
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        color: 'black',
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                    }}>
+                        Grįžti
+                    </Text>
+                </TouchableOpacity>
+            </NavBase>
+        );
+    }
+
 }
